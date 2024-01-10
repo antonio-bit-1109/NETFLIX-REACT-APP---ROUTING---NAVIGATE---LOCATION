d58dd2f4e2920fc1628aaab712b4bef9d554b146
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-10 22:55:50 +0100

    gg

--- netflix-react-app-routing-location-last-update/src/Components/CommentsComponent.jsx
+++ netflix-react-app-routing-location-last-update/src/Components/CommentsComponent.jsx
@@ -6,27 +6,27 @@ const CommentsComponent = (props) => {
 
     return (
         <div>
-            {
-                <Col>
-                    {" "}
-                    <ListGroup>
-                        <h3>Lista dei commenti: </h3>
-                        {comments.slice(0, 5).map((comment, index) => (
-                            <>
-                                <div key={`comm-${index}`} className=" border border-primary my-3">
-                                    {" "}
-                                    <ListGroup.Item>{comment.author}</ListGroup.Item>
-                                    <ListGroup.Item>{comment.comment}</ListGroup.Item>
-                                    <ListGroup.Item>{comment.elementId}</ListGroup.Item>
-                                </div>
-                                <div>
-                                    <Button variant="danger">Cancella Commento </Button>
-                                </div>
-                            </>
-                        ))}
-                    </ListGroup>
-                </Col>
-            }{" "}
+            <Col>
+                {" "}
+                <ListGroup>
+                    <h3>Lista dei commenti: </h3>
+                    {comments.slice(0, 5).map((comment, index) => (
+                        <>
+                            <div key={`comm-${index}`} className=" border border-primary my-3">
+                                {" "}
+                                <ListGroup.Item>{comment.author}</ListGroup.Item>
+                                <ListGroup.Item>{comment.comment}</ListGroup.Item>
+                                <ListGroup.Item>{comment.elementId}</ListGroup.Item>
+                            </div>
+                            <div>
+                                <Button type="submit" variant="danger">
+                                    Cancella Commento{" "}
+                                </Button>
+                            </div>
+                        </>
+                    ))}
+                </ListGroup>
+            </Col>
         </div>
     );
 };
