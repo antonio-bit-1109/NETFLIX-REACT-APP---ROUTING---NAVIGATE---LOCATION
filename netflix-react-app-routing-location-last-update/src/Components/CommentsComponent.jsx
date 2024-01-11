478bd84c6e903d691c3fd8315c36f05078cc1045
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-11 08:39:02 +0100

    delete fetch

--- netflix-react-app-routing-location-last-update/src/Components/CommentsComponent.jsx
+++ netflix-react-app-routing-location-last-update/src/Components/CommentsComponent.jsx
@@ -3,7 +3,48 @@ import { Button, Col, ListGroup } from "react-bootstrap";
 
 const CommentsComponent = (props) => {
     const { comments } = props;
+    console.log("comments", comments);
     /* SCRIVI LA FETCH PER FARE UNA DELETE  */
+
+    const fetchDelete = (value) => {
+        const optionsComments = {
+            method: "DELETE",
+            headers: {
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTcxY2JhMDBkOGEyMDAwMThhNDhhNDAiLCJpYXQiOjE3MDQ5MDEwMzksImV4cCI6MTcwNjExMDYzOX0.JgBLrx0gtEMbqKZEL4v4DHn-u2liDfnO58fki3MUC1Q",
+            },
+            "Content-Type": "application/json",
+        };
+
+        const UrlToGetComments = `https://striveschool-api.herokuapp.com/api/comments/${value}`;
+
+        fetch(UrlToGetComments, optionsComments)
+            .then((response) => {
+                if (!response.ok) {
+                    if (response.status > 400 && response.status < 500) {
+                        if (response.status === 429) {
+                            throw new Error("429 INFAME PER TE TANTE COSE BRUTTE");
+                        } else {
+                            throw new Error("STAI CAPPELLANDO , RIGUARDA QUELLO CHE HAI SCRITTO");
+                        }
+                    }
+                    if (response.status > 500 && response.status < 600) {
+                        throw new Error("SERVER SPOMPATO, NON FUNZIA??");
+                    }
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+
+            .catch((err) => console.error(err));
+    };
+
+    const handleDelete = () => {
+        fetchDelete(comments._id);
+    };
 
     return (
         <div>
@@ -20,7 +61,7 @@ const CommentsComponent = (props) => {
                                 <ListGroup.Item>{comment.elementId}</ListGroup.Item>
                             </div>
                             <div>
-                                <Button type="submit" variant="danger">
+                                <Button onClick={handleDelete} type="button" variant="danger">
                                     Cancella Commento{" "}
                                 </Button>
                             </div>
