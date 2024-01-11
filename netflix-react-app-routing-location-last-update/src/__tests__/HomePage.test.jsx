131ec42b607cd2c72f1941b8e435aae01f1736fc
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-11 15:25:02 +0100

    gg

--- netflix-react-app-routing-location-last-update/src/__tests__/HomePage.test.jsx
+++ netflix-react-app-routing-location-last-update/src/__tests__/HomePage.test.jsx
@@ -1,1 +1,17 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import App from "../App";
+
+/* cartella contenente i test  */
+
+describe("correct mounting of h1", () => {
+    it("mount the h1 correctly", () => {
+        /* ub quale componente si trova l'elemento da testare? */
+        render(<App />);
+
+        /* lo acchiappo dal virtual DOM */
+        const h1Prova = screen.getByText(/Sempre/i);
+
+        /* cosa mi aspetto ?? */
+        expect(h1Prova).toBeInTheDocument();
+    });
+});
